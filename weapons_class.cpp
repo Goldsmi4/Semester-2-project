5864a8fba68fa6034423208c7602ab050aeff0e1
William Escobar-Parra <escobarw@uni.coventry.ac.uk>
2017-02-20 12:23:46 +0000

    added weapons class for database

---
+++ weapons_class.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class weapons {
+    public:
+        string type; // weapon type
+        string name; //name of weapon
+        int condition; //weapons condition between 1-10 (randomised)
+        int attack; //weapon attack value
+
+};
+
