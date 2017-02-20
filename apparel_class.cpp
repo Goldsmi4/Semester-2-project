9a4a23e3d7acaa739b73bbbcde5fd80ef3727601
Timothy Goldsmith <goldsmi4@uni.coventry.ac.uk>
2017-02-20 12:52:56 +0000

    Class for apparel

---
+++ apparel_class.cpp
@@ -0,0 +1,24 @@
+#ifndef apparel_class_H
+#define apparel_class_H
+/* Classes for apparel */
+
+#include <iostream>
+using namespace std;
+
+
+class apparel {
+    public:
+        string type;        // apparel type
+        int condition;      // condition of equipment
+};
+
+int main() {
+    
+}
+    
+
+    
+
+
+
+#endif
