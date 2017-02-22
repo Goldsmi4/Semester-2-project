798ec6d00fa035cf3a8abcb83a6dc88f4977e925
William Escobar-Parra <escobarw@uni.coventry.ac.uk>
2017-02-22 18:06:10 +0000

    added game start

---
+++ game_start.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include "game.h"
+#include "game_title.h
+#include "main_story.h"
+#include "player_input.h"
+
+using namespace std;
+
+
+int main()
+    
+{
+    cout << "\n"+ game_title()<< endl ;
+    
+    
+    
+    cout << "do you want to play quarantine zombie survival? " << endl;
+    bool decision;
+    decision = answer();
+    if (decision) {
+        cout << "shit hits the fan welcome to hell.\n" << endl;
+        start_game(); //begin quarantine
+    }
+    
+}
