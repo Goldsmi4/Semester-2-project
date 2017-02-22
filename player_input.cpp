562b2dffd8ab7cbfa0022a229c12b440da477f77
William Escobar-Parra <escobarw@uni.coventry.ac.uk>
2017-02-22 18:07:39 +0000

    player input polymorphism to user throughout

---
+++ player_input.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool answer()
+{
+    cout<< "enter at own discretion...(y/n): "<<endl ;
+    
+    string entry = "";
+    bool valid_entry = false;
+    
+    while (!valid_entry) {
+        cin >> entry;
+        
+        if (entry =="yes" or entry == "Yes" or entry == "y" or entry == "Y") {
+            valid_entry= true;
+            return true;
+        }else if (entry =="No" or entry == "no" or entry == "N" or entry == "n"{
+            valid_entry = true;
+            return false;    
+        }else{
+            cout << "invalid input! please try again " << endl;
+            
+        }
+    }
+        
+}
+    
