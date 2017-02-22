a2d4fc8aedffe734a10b4d02e69d5b478200a2a9
William Escobar-Parra <escobarw@uni.coventry.ac.uk>
2017-02-22 18:09:11 +0000

    just testing

---
+++ main_story.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include "game.h"
+#include "game_title.h"
+
+void start_game(){
+    cout << " hello you haave reached the right place high five5 " << endl;
+}
