3357f7305b7c0bf5b72842702e9aea48e7baacc5
Timothy Goldsmith <goldsmi4@uni.coventry.ac.uk>
2017-03-06 10:44:14 +0000

    no changes made

--- apparel_class.cpp
+++ apparel_class.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class apparel {
     public:
         string type;        // apparel type
-        int condition;      // condition of equipment
+        int condition;      // condition of equipment value 1-10
 };
 
 int main() {
