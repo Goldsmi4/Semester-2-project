dd3e130b3b913f367ef22c7500a47d0297cdc9d8
Dilraj Sahota-Punnia <punniad@uni.coventry.ac.uk>
2017-03-09 18:41:01 +0000

    dilraj sahota story path

--- story_outside/story_outside.cpp
+++ story_outside/story_outside.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
 void(outside)
 
-system (outside)
-    got
+{
+    int outside_path;
+    
+    cout << "you walk outside and see multiple paths that lead to different districts" << endl;
+    cout << "Which path will you take?" << endl;
+    
+    cout << "/t >> 1: 
