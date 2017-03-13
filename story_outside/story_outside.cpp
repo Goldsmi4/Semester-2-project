490f610f518314565ae2a2c2864546f507840bfa
Dilraj Sahota-Punnia <punniad@uni.coventry.ac.uk>
2017-03-13 13:49:21 +0000

    updated code

--- story_outside/story_outside.cpp
+++ story_outside/story_outside.cpp
@@ -13,4 +13,33 @@ void(outside)
     cout << "you walk outside and see multiple paths that lead to different districts" << endl;
     cout << "Which path will you take?" << endl;
     
-    cout << "/t >> 1: 
+    cout << "\t >> 1: A dark alleyway" << endl;
+    cout << "\t >> 2: follow a deserted road" << endl;
+    cout << "\t >> 3: Go to the nearest building" << endl;
+    cout << "\t >> 4: Go to to an abandoned park" << endl;
+ 
+    
+    if(outside_path == 1)
+    {
+
+    }
+    else if(outside_path == 2)
+    {
+
+    }
+    else if(outside_path == 3)
+    {
+
+    }
+    else if(outside_path == 4)
+    
+    else
+    {
+     cout << "enter valid input " << endl;
+        goto retry;   
+    }
+ 
+    cout << "\n----------------------Press key to continue----------------------" << endl;
+    _getch();
+    
+}
