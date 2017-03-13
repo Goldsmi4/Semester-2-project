58e92eb38b363c25a07e211859ce114761e7c6b1
Dilraj Sahota-Punnia <punniad@uni.coventry.ac.uk>
2017-03-13 13:49:47 +0000

    old backup of code

---
+++ storySupplies/old_text.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void (Supplies)
+
+
+
+{
+    int supplies_path;
+    
+    cout << "you look around the house and grab everything you can realising" << endl;
+    cout << "that everything is too heavy to carry with you.   " << endl;
+    cout << "what do you priorities first ?" << endl;
+    
+    cout << "\t >> 1: weapons" << endl;
+    cout << "\t >> 2: food" << endl; //
+    cout << "\t >> 3: clothes" << endl;
+    cout << "\t >> 4: electronics" << endl;
+    
+
+    if(supplies_path == 1)
+    {
+
+    }
+    else if(supplies_path == 2)
+    {
+
+    }
+    else if(supplies_path == 3)
+    {
+
+    }
+    else if(supplies_path == 4)
+    
+    else
+    {
+     cout << "enter valid input " << endl;
+        goto retry;   
+    }
+ 
+    cout << "\n----------------------Press key to continue----------------------" << endl;
+    _getch();
+    
