808e9ce913cf1f178615cd5ed521ac0b0d57bcd7
Abdulkadir Maxamed <qaasima@uni.coventry.ac.uk>
2017-03-13 11:32:34 +0000

    updated

--- call_family.cpp
+++ call_family.cpp
@@ -13,16 +13,42 @@ int main()
     return(0);
 }
 
+void temp()
+    
+
+
+void otheroptions(){
+    int input;
+    {
+        cout << "Please select an option:" << endl
+        << "1.love oskars " << endl
+        << "2.Go to neighbours " << endl
+        << "3.Hide " << endl
+        << "4.Shout for help " << endl;
+        cin >> input;
+        
+       switch(input)
+       {
+           case 1:
+               cout<<"love oskars"<< endl;
+               break;
+               
+       }
+          
+    }
+}
+    
+
 void options()
 {
     int input;
+    
     
     {
         cout << "Please select an option:" << endl
         << "1.Search the house " << endl
         << "2.Go to neighbours " << endl
-        << "3.Hide " << endl
-        << "4.Shout for help " << endl;
+        << "3.Hide " << endl;
         cin >> input;
         
        switch(input)
@@ -31,20 +57,24 @@ void options()
                exit(0);
            case 1:
                cout << "Search the house" << endl;
-               cout <<"u die"<<endl;
+               otheroptions();
+               
                break;
            case 2:
                cout << "Go to neighbours" << endl;
                break;
            case 3:
                cout << "Hide " << endl;
+               
                break;
            case 4:
                cout << "Shout for help" <<endl;
                break;
            default:
                cout<<"invalid option please choose between 1 and 4"<< endl;
-               break; 
+         }
+    
+               
        }
-    }
 }
+
