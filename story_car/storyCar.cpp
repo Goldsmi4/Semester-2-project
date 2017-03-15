286cb558bb6cfc911f6b6a088d6fec6c88a0f67d
Timothy Goldsmith <goldsmi4@uni.coventry.ac.uk>
2017-03-15 19:32:17 +0000

    update to my code (bugs)

--- story_car/storyCar.cpp
+++ story_car/storyCar.cpp
@@ -1,79 +1,199 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-#include "game.h"
-#include "game_title.h"
+
 using namespace std;
 
-void (Car)
-
-system('cls');
-
+int main()
+//int car()
 {
-    int car_path;
-    cout << "\n!!!----------------------Chapter One: Escape----------------------!!!" << endl;
-    cout << "You grab your car keys and whatever is to hand. You then head out to the" << endl;
-    cout << "car and get in. After switching the engine on you see that there is little" << endl;
-    cout << "fuel left in the tank. What should you do?" << endl;
-    
-   
-    cout << "\t >> 1: Look in the garage for fuel" << endl;
-    cout << "\t >> 2: Try using a neighbours cars, hope it has more fuel" << endl; //
-    cout << "\t >> 3: ignore it, there should be enough" << endl;
-    
-    
-    if(car_path == 1)
+
+    int car();
     {
-    cout << "You go into the garage and look around and see fuel cans, a knife and some duck tape." << endl;
-    cout << "You pick these items up and go back to the car." << endl;
-    cout << "There isn't much fuel in the fuel cans but you pour what is there into the tank and " << endl;
-    cout << "and put the empty cans into the boot." << endl;
-    cout << "You get back into the car and see the tank is now 1/3 full." << endl;
-    cout << " you start driving"
-    }
-    else if(car_path == 2)
-    {
-    int Neighbour_path;
-    cout << "You leave the keys in the ignition of your car and go to your neighbour's house. You knock" << endl;
-    cout << "on the front door and there no response inside." << endl;
-    cout << "How do you want to enter?" << endl;
+        cout << "You grab your car keys and whatever is to hand. You then head down to the" << endl;
+        cout << "car and get in. After switching the engine on you see that there is little" << endl;
+        cout << "fuel left in the tank. What should you do?" << endl;
+        cout << "\n" << endl;
+        cout << "/t 1: Look in the garage for fuel", "red" << endl;
+        cout << "/t 2: Try using a neighbours car, hope it has more fuel", "red" << endl; //
+        cout << "/t 3: ignore it, there should be enough", "red" << endl;
+        cin >> input;
+        switch (input)
+        {   
+    
+            case 1:    
+                    cout << "You go into the garage and look around and see fuel cans, a knife and some duck tape." << endl;
+                    cout << "You pick these items up and go back to the car." << endl;
+                    cout << "There isn't much fuel in the fuel cans but you pour what is there into the tank and " << endl;
+                    cout << "and put the empty cans into the boot." << endl;
+                    cout << "You get back into the car and see the tank is now 1/3 full." << endl;
+                    cout << " you start driving" << endl;
+                    route_path();
+                    break;
         
-    cout << "/t >> 1: Shout through the letterbox to see if there is anyone home" << endl;
-    cout << "/t >> 2: Sneak in through the backdoor" << endl;
-    cout << "/t >> 3: Kick the door down" << endl;
-    cout << "/t >> 4: return to your car"
     
-    }
-    else if(car_path == 3)
-    {
-    cout << "you get into the car and start driving" << endl;
-    }
-       
-    else
-    {
-     cout << "enter valid input " << endl;
-        goto retry;   
-    }
- 
-    cout << "\n----------------------Press key to continue----------------------" << endl;
-    _getch();
     
+            case 2:
+                    cout << "You leave the car and head over to your neighbour's house." << endl;
+                    
+                    neighbourCar();
+                    break;
+    
+    
+            case 3:
+                    cout << "you get into the car and start driving" << endl;
+                    route_path();
+                    break;
+        
+        }
+    }
 }
-    int route_path;
-    cout << "ou reach the edge of the neighbourhood, which route do you want to take?" << endl;
-
-    cout << "/t >> 1: Take the ring road and travel around"
-   
+    /*else
+    {
+        
+    cout << " Thats not an option, choose one from the list. " << endl;
+    goto retry;
+        
+    }*/
+ 
+void main();
+//void car()
+int neighbourCar();// <------------------------------
 {
     
+    {
+        cout << "You go up to the front door. how should you enter?" << endl;
+        cout << "\n" << endl;
+        cout << "/t 1: Front door" << endl;
+        cout << "/t 2: Back door" << endl;
+        cout << "/t 3: Leave it and go back to your car" << endl;
+        cin >> input;
+        switch (input)
+        {
+            case 1:
+                cout << "you break the window in the front door. A looter inside was watching the window" << endl;
+                cout << "and shoots you in the head. you are dead." << endl;
+                cout << "\n Game over" << endl;
+                
+                break;
+            case 2:
+                cout << "You sneak around to the back door and see it has been forced open. You sneak in" << endl;
+                cout << "and see some guy, not your neighbour, watching the front door." << endl;
+                
+                killLooter();
+                break;
+                
+            case 3:   
+                
+                break;
+        }
+        
+    }
+    
+    
+}
+    
+void main();
+// void car()7
+{
+    int killLooter();
+    {
+        cout << "He doesnt notice you but he is armed with a pistol. Do you quietly kill him?" << endl;
+        cout << "\n" <<endl;
+        cout << "/t 1: Yes" << endl;
+        cout << "/t 2: No and leave" << endl;
+        cin >> input;
+        switch (input)
+        {
+            case 1:
+                cout << "You sneak up behind him and kill them. You loot him and get his pistol" << endl;
+                cout << "along with a tin of food. You rummage around the house and find some old" << endl;         // possibly change this bit to use Will's search house code
+                cout << "tshirts you can use as rags and some magazines you could use as bite protection" << endl;
+                cout << "on your arms." << endl;
+                break;
+            case 2:
+                cout << "You sneak out and leave the house, you go straight to your car and drive off."
+                
+                route_path();
+                break;
+                
+                
+        }
+    }
 }
 
 
+ 
+void main();
+//void car()   
+{
+    int route_path();
+    {
+        cout << "You reach the edge of the neighbourhood, which route do you want to take?" << endl;
+        cout << "\n" << endl;
+        cout << "/t 1: Take the ring road and travel around to the evacuation point. It could be safer but you are less likley to find anything" << endl;
+        cout << "/t 2: Travel straight to the evacuation point. This will take you through residential areas but there is more chance to find things" << endl;
+        cout << "/t 3: Go to the evacuation point via the gunshop downtown. There could be weapons there"
+        cin >> input;
+        switch (input)
+        {    
+            case 1:
+    
+    
+                cout << "You travel along the ringroad towards the evacuation point, whilst on the overpass you see a roadblock up ahead." << endl;
+                cout << "You stop the car and get out well short of the roadblock. you are close to the evacuation point" << endl;
+                cout << "and there are no other routes to take. So you approach the roadblock carefully. There seems to be noone there." <<endl;
+                
+                //ringroadShop();
+       
+                break;
 
+            case 2:
+    
+    
+                cout << "You travel down the main road that leads to the evacuation point. You turn around a sharp" << endl;
+                cout << "corner and see the road is blocked and theres people sitting on the blockade. Before you" << endl;
+                cout << "Can do anything hey shoot the windscreen and hit you in the chest. You are dead."
+                cout<< "/n game over please start again" << endl;
+                
+                break;
+    
+            case 3:
+                cout << "you Drive down the road that leads to the gunshop." << endl;
+                
+                lootGunshop()
+                
+                break;
+    
+        }
+    }
+}
 
+int lootGunshop()
+{
+    cout << "You reach the shop with no problems but you have attracted a group of zombies a few hundred meters behind you." << endl;
+    cout << "The shop looks clear but you dont have long. do you loot the gunshop?" << endl;
+    cout << "\n" << endl;
+    cout << "/t 1: yes" <<endl;
+    cout << "/t 2: no, they are too close." << endl;
+    cin >> input;
+    switch (input)
+    {
+        case 1:
+            cout << "" << endl;
+            
+            
+            break;
+        case 2:
+            cout << "" << endl;
+            
+            
+            break;
+    }
+}
 
+    
 
 
 
