d0001cb45281f2918e74c347fd3480f82fb29d11
Dilraj Sahota-Punnia <punniad@uni.coventry.ac.uk>
2017-03-16 13:33:59 +0000

    old text for outside path

---
+++ story_outside/old_text_for_story_outside.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+
+int main()
+//void (outside)
+
+{
+    int outside_path;
+    
+    
+    cout << "you walk outside and see multiple paths that lead to different districts" << endl;
+    cout << "Which path will you take?" << endl;
+    
+    cout << "\t >> 1: A dark alleyway" << endl;
+    cout << "\t >> 2: follow a deserted road" << endl;
+    cout << "\t >> 3: Go to the nearest building" << endl;
+    cout << "\t >> 4: Go to to an abandoned park" << endl;
+ 
+    
+    cout << "Enter your choice: ";
+    cin >> outside_path;
+    
+    
+    
+      if(outside_path == 1)
+    {
+      cout << "you follow the alleyway path and discover it is a dead end, turn back!" <<endl;
+    }
+    else if(outside_path == 2)
+    {
+      cout << " you go down that deserted road and discover 
+    }
+    else if(outside_path == 3)
+    {
+
+    }
+    else if(outside_path == 4)
+    
+    {
+        
+    }
+    
+    else
+    {
+     cout << "enter valid input " << endl;
+        
+    }
+ 
+    cout << "\n----------------------Press key to continue----------------------" << endl;
+    
+    
+}
+
